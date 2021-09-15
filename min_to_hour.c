2327757417e8879fbb01ff39d8599bccef2d0e1e
alairack <1477115009@qq.com>
2021-09-15 10:51:13 +0800

    the first version.basic function.

---
+++ min_to_hour.c
@@ -0,0 +1,28 @@
+/*把用分钟表示的时间转换成用小时和分钟表示的时间, learnc */
+#include <stdio.h>
+#define hour 60
+
+int main(void)
+{
+	int time;
+	int hr;
+	int min;
+	while (1)
+	{
+		printf("please input time.\n");
+		if (scanf("%d", &time) <= 0)
+		{
+			printf("do not input error char!\n");
+			break;
+		}
+		if (time <= 0)
+			break;
+		else
+		{
+			hr = time / hour;
+			min = time % hour;
+		}
+		printf("%d h %d min\n", hr, min);
+	}
+	return 0;
+}
