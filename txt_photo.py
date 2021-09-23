ba9d774f86b4e290c231218340922aa334976adf
alairack <1477115009@qq.com>
2021-09-23 19:57:07 +0800

    去掉了一些冗余的函数与变量，优化了字符转换机制，由list代替原有的dict

--- txt_photo.py
+++ txt_photo.py
@@ -3,33 +3,27 @@ import sys
 
 def main():
     namespace = locals()
-    f = open(sys.argv[1], "r")
-    save = open("output.txt", "w")
-    content = f.read()
-    content = content.replace(" ", "")
-    y = 0
-    x = 0
-    namespace["content_0"] = []
-    for ch in content:
-        if ch == "\n":
-            x = x + 1
-            y = 0
-            namespace["content_" + str(x)] = []
-            save.write("\n")
-            continue
-        ch = convert_char(ch)
-        save.write(ch)
-        print(ch, end="")
-        namespace["content_" + str(x)].append(ch)
-        y = y + 1
-    f.close()
-    save.close()
+    with open(sys.argv[1], "r") as f:
+        with open("output.txt", "w") as save:
+            content = f.read()
+            content = content.replace(" ", "")
+            convert = [" ", ".", "`", ":", "~", "*", "=", "&", "%", "#"]
+            x = 0
+            namespace["content_0"] = []
+            for ch in content:
+                if ch == "\n":
+                    x = x + 1
+                    namespace["content_" + str(x)] = []
+                    save.write("\n")
+                    continue
+                try:
+                    ch = convert[int(ch)]
+                except IndexError:
+                    raise ValueError
+                save.write(ch)
+                print(ch, end="")
+                namespace["content_" + str(x)].append(ch)
     return 0
-
-
-def convert_char(character):
-    convert = {'0': " ", '1': ".", '2': "`", '3': ":", '4': "~", '5': "*", '6': "=", '7': "&", '8': "%", '9': "#"}
-    return convert[character]
 
 
 if __name__ == "__main__":
@@ -41,5 +35,6 @@ if __name__ == "__main__":
     except FileNotFoundError:
         print("file not found!\n")
         exit(3)
-    except KeyError:
-        print("the numbers of file is unsupported!\n")
+    except ValueError:
+        print("the numbers in file is unsupported!\n")
+        exit(4)
